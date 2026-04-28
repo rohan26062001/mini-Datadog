59abd3d24775fd46b3f3a9853b7f474efcde43a3
Rohan Das <bornforcoding777@gmail.com>
2026-04-28 09:28:31 +0530

    Day 1: Project structure + ingestion-service setup

---
+++ backend/common-lib/src/main/java/com/observability/common/dto/LogEvent.java
@@ -0,0 +1,8 @@
+package com.observability.common.dto;
+
+public class LogEvent {
+    private String serviceName;
+    private String level;
+    private String message;
+    private long timestamp;
+}
